7afbafabcefa5f1da9fe0ec27b424f8c861d5e41
Ondřej Mottl <60345393+OndrejMottl@users.noreply.github.com>
2026-03-12 14:41:01 +0100

    Add initial presentation and learning materials (#1)

---
+++ R/render_presentation.R
@@ -0,0 +1,51 @@
+#----------------------------------------------------------#
+#
+#
+#                     L - example
+#
+#                  Render presentation
+#
+#
+#                       O. Mottl
+#                         2026
+#
+#----------------------------------------------------------#
+
+# Setup -----
+
+library(here)
+library(quarto)
+library(fs)
+
+# Render -----
+quarto::quarto_render(
+  input = here::here("Presentation/presentation.qmd")
+)
+
+# Move the rendered file to the `docs` directory. -----
+
+fs::file_copy(
+  path = here::here("Presentation/presentation.html"),
+  new_path = here::here("docs/index.html"),
+  overwrite = TRUE
+)
+
+# Make PDF version -----
+
+# decktape needs to be installed separately.
+# See https://github.com/astefanutti/decktape
+system2(
+  command = "decktape.cmd",
+  args = c(
+    "reveal", "--fragments=false", # make --fragments=true will make the PDF look weird
+    "--size 1050x700",
+    here::here("Presentation/presentation.html"),
+    here::here("Presentation/presentation_raw.pdf")
+  )
+)
+
+# compress the PDF to make it small enough to uplot to GH
+qpdf::pdf_compress(
+  input = here::here("Presentation/presentation_raw.pdf"),
+  output = here::here("Presentation/presentation.pdf")
+)
